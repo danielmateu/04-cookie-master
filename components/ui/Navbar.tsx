bbd434b1b7059a7ddd4e74188a8f6a3bbff6d182
Daniel Mateu <danielmateu86@gmail.com>
2022-09-28 12:01:22 +0200

    Layout y paginas adicionales creadas

---
+++ components/ui/Navbar.tsx
@@ -0,0 +1,33 @@
+import NextLink from 'next/link';
+
+
+import { AppBar, IconButton, Toolbar, Typography } from '@mui/material'
+import {MenuOutlined} from '@mui/icons-material';
+import Link from 'next/link';
+
+export const Navbar = () => {
+    return (
+        <AppBar position='sticky' elevation={0}>
+            <Toolbar>
+                <IconButton
+                    size='large'
+                    edge='start'>
+                        <MenuOutlined/>
+                </IconButton>
+
+                <NextLink href='/' passHref>
+                    <Link href={'/'} >
+                        <Typography variant='h6' color='white'>Cookie-Master</Typography>
+                    </Link>
+                </NextLink>
+                <div style={{flex: 1}}></div>
+                <NextLink href='/theme-changer' passHref>
+                    <Link href={'/'}>
+                        <Typography variant='h6' color='white'>Cambiar tema</Typography>
+                    </Link>
+                </NextLink>
+            </Toolbar>
+
+        </AppBar>
+    )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,12 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import { Layout } from '../components/layouts'
+
 
 const Home: NextPage = () => {
   return (
-    <h1>Cookie Master! 👾</h1>
+    <Layout>
+      <h1>Cookie Master</h1>
+    </Layout>
   )
 }
 
